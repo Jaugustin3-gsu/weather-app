af39b5bf85eaf4672a4cc80a9fe9c059bd19f4e1
Jarhud <jaugustin3@student.gsu.edu>
2024-09-25 22:53:41 -0400

    Final testing and adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -72,21 +72,23 @@ void _fetchWeather(){
     cityName = _controller.text ;
     
 
-    if(randomCond == 1 )
+    if(randomCond == 1 ) {
       condition = "sunny";
+    }
 
-    if(randomCond == 2 )
+    if(randomCond == 2 ) {
       condition = "cloundy";
-      
-    if(randomCond == 3 )
+    }
+
+    if(randomCond == 3 ) {
       condition = " rainy";
+    }
 
 
   });
 
 }
   
-
   @override
   void initState() {
     super.initState();
@@ -96,41 +98,20 @@ void _fetchWeather(){
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+   
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+      
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        
         title: Text(widget.title),
       ),
       // ignore: prefer_const_constructors
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      
         // ignore: prefer_const_constructors
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+         
           mainAxisAlignment: MainAxisAlignment.center,
           // ignore: prefer_const_literals_to_create_immutables
           children: <Widget>[
@@ -138,7 +119,7 @@ void _fetchWeather(){
              TextField(
               controller: _controller,
               obscureText: true,        
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'City Name',)
               ),
